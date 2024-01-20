68a3d8b817151f147d766759d8818ba1b685bab9
okari <nyandika15@gmail.com>
2024-01-20 19:05:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,57 +2,517 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{width: 1440, height: 1080, position: 'relative', background: 'white', boxShadow: '0px 2px 24px rgba(0, 0, 0, 0.10)', borderRadius: 8, overflow: 'hidden'}}>
-  <div style={{left: 77, top: 40, position: 'absolute', color: '#333333', fontSize: 24, fontFamily: 'Inconsolata', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word'}}>404 not found</div>
-  <div style={{width: 54.78, height: 9.39, left: 736, top: 316.86, position: 'absolute', border: '2px white solid'}}></div>
-  <div style={{width: 586, left: 735, top: 265, position: 'absolute', color: '#333333', fontSize: 64, fontFamily: 'Space Mono', fontWeight: '700', wordWrap: 'break-word'}}>I have bad news for you</div>
-  <div style={{width: 381, left: 736, top: 491, position: 'absolute', color: '#4F4F4F', fontSize: 24, fontFamily: 'Space Mono', fontWeight: '400', wordWrap: 'break-word'}}>The page you are looking for might be removed or is temporarily unavailable</div>
-  <div style={{width: 216, height: 68, left: 740, top: 663, position: 'absolute'}}>
-    <div style={{width: 216, height: 68, left: 0, top: 0, position: 'absolute', background: '#333333'}} />
-    <div style={{left: 43, top: 24, position: 'absolute', color: 'white', fontSize: 14, fontFamily: 'Space Mono', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word'}}>Back to homepage</div>
-  </div>
-  <div style={{width: 539.22, height: 447.43, left: 77, top: 265, position: 'absolute'}}>
-    <div style={{width: 8.79, height: 301.85, left: 254.47, top: 145.58, position: 'absolute', background: '#2B254E'}}></div>
-    <div style={{width: 539.22, height: 414.25, left: 0, top: 0, position: 'absolute', background: '#FFD25B'}}></div>
-    <div style={{width: 62.27, height: 65.69, left: 233.22, top: 145.37, position: 'absolute', background: '#FFD25B'}}></div>
-    <div style={{width: 116.02, height: 147.30, left: 187.78, top: 206.39, position: 'absolute'}}>
-      <div style={{width: 116.02, height: 147.30, left: 0, top: 0, position: 'absolute', background: 'white'}}></div>
-      <div style={{width: 116.02, height: 147.30, left: 0, top: 0, position: 'absolute', background: '#FF6D5B'}}></div>
-      <div style={{width: 29.17, height: 49.24, left: 58.95, top: 112.44, position: 'absolute', transform: 'rotate(-177deg)', transformOrigin: '0 0', background: '#FFD25B'}}></div>
-    </div>
-    <div style={{width: 370.96, height: 148.97, left: 83.77, top: 145.58, position: 'absolute', background: '#2B254E'}}></div>
-    <div style={{width: 463.52, height: 7.08, left: 37.61, top: 169.76, position: 'absolute', background: '#847DAA'}}></div>
-    <div style={{width: 49.51, height: 102.97, left: 254.49, top: 145.95, position: 'absolute', background: '#60588E'}}></div>
-    <div style={{width: 169.73, height: 56.69, left: 187.31, top: 76.71, position: 'absolute', background: '#2B254E'}}></div>
-    <div style={{width: 41.76, height: 19.33, left: 231.27, top: 142.65, position: 'absolute', background: '#847DAA'}}></div>
-    <div style={{width: 107.95, height: 84.94, left: 215.10, top: 66.12, position: 'absolute'}}>
-      <div style={{width: 107.95, height: 84.94, left: 0, top: 0, position: 'absolute', background: 'white'}}></div>
-      <div style={{width: 107.95, height: 84.94, left: 4.73, top: -5.41, position: 'absolute', transform: 'rotate(6deg)', transformOrigin: '0 0', background: '#FF6D5B'}}></div>
-    </div>
-    <div style={{width: 74.36, height: 27.53, left: 236.53, top: 89.80, position: 'absolute', background: 'white'}}></div>
-    <div style={{width: 76.04, height: 38.74, left: 233.83, top: 97.02, position: 'absolute', background: '#2B254E'}}></div>
-    <div style={{width: 38.59, height: 26.98, left: 256.67, top: 33.13, position: 'absolute', background: '#60588E'}}></div>
-    <div style={{width: 169.73, height: 71.76, left: 187.31, top: 37.18, position: 'absolute'}}>
-      <div style={{width: 169.73, height: 71.76, left: 0, top: 0, position: 'absolute', background: 'white'}}></div>
-      <div style={{width: 169.73, height: 71.76, left: 0, top: 0, position: 'absolute', background: '#60588E'}}></div>
-      <div style={{width: 44.69, height: 34.19, left: 49.33, top: 1.71, position: 'absolute'}}>
-        <div style={{width: 44.69, height: 34.19, left: 0, top: 0, position: 'absolute', background: 'white'}}></div>
-        <div style={{width: 44.69, height: 34.19, left: 0, top: 0, position: 'absolute', background: '#2B254E'}}></div>
-        <div style={{width: 36.14, height: 31.26, left: 15.63, top: 6.59, position: 'absolute', background: '#FFD25B'}}></div>
+    <div
+      style={{
+        width: 1440,
+        height: 1080,
+        position: 'relative',
+        background: 'white',
+        boxShadow: '0px 2px 24px rgba(0, 0, 0, 0.10)',
+        borderRadius: 8,
+        overflow: 'hidden',
+      }}
+    >
+      <div
+        style={{
+          left: 77,
+          top: 40,
+          position: 'absolute',
+          color: '#333333',
+          fontSize: 24,
+          fontFamily: 'Inconsolata',
+          fontWeight: '700',
+          textTransform: 'uppercase',
+          wordWrap: 'break-word',
+        }}
+      >
+        404 not found
+      </div>
+      <div
+        style={{
+          width: 54.78,
+          height: 9.39,
+          left: 736,
+          top: 316.86,
+          position: 'absolute',
+          border: '2px white solid',
+        }}
+      ></div>
+      <div
+        style={{
+          width: 586,
+          left: 735,
+          top: 265,
+          position: 'absolute',
+          color: '#333333',
+          fontSize: 64,
+          fontFamily: 'Space Mono',
+          fontWeight: '700',
+          wordWrap: 'break-word',
+        }}
+      >
+        I have bad news for you
+      </div>
+      <div
+        style={{
+          width: 381,
+          left: 736,
+          top: 491,
+          position: 'absolute',
+          color: '#4F4F4F',
+          fontSize: 24,
+          fontFamily: 'Space Mono',
+          fontWeight: '400',
+          wordWrap: 'break-word',
+        }}
+      >
+        The page you are looking for might be removed or is temporarily
+        unavailable
+      </div>
+      <div
+        style={{
+          width: 216,
+          height: 68,
+          left: 740,
+          top: 663,
+          position: 'absolute',
+        }}
+      >
+        <div
+          style={{
+            width: 216,
+            height: 68,
+            left: 0,
+            top: 0,
+            position: 'absolute',
+            background: '#333333',
+          }}
+        />
+        <div
+          style={{
+            left: 43,
+            top: 24,
+            position: 'absolute',
+            color: 'white',
+            fontSize: 14,
+            fontFamily: 'Space Mono',
+            fontWeight: '700',
+            textTransform: 'uppercase',
+            wordWrap: 'break-word',
+          }}
+        >
+          Back to homepage
+        </div>
+      </div>
+      <div
+        style={{
+          width: 539.22,
+          height: 447.43,
+          left: 77,
+          top: 265,
+          position: 'absolute',
+        }}
+      >
+        <div
+          style={{
+            width: 8.79,
+            height: 301.85,
+            left: 254.47,
+            top: 145.58,
+            position: 'absolute',
+            background: '#2B254E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 539.22,
+            height: 414.25,
+            left: 0,
+            top: 0,
+            position: 'absolute',
+            background: '#FFD25B',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 62.27,
+            height: 65.69,
+            left: 233.22,
+            top: 145.37,
+            position: 'absolute',
+            background: '#FFD25B',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 116.02,
+            height: 147.3,
+            left: 187.78,
+            top: 206.39,
+            position: 'absolute',
+          }}
+        >
+          <div
+            style={{
+              width: 116.02,
+              height: 147.3,
+              left: 0,
+              top: 0,
+              position: 'absolute',
+              background: 'white',
+            }}
+          ></div>
+          <div
+            style={{
+              width: 116.02,
+              height: 147.3,
+              left: 0,
+              top: 0,
+              position: 'absolute',
+              background: '#FF6D5B',
+            }}
+          ></div>
+          <div
+            style={{
+              width: 29.17,
+              height: 49.24,
+              left: 58.95,
+              top: 112.44,
+              position: 'absolute',
+              transform: 'rotate(-177deg)',
+              transformOrigin: '0 0',
+              background: '#FFD25B',
+            }}
+          ></div>
+        </div>
+        <div
+          style={{
+            width: 370.96,
+            height: 148.97,
+            left: 83.77,
+            top: 145.58,
+            position: 'absolute',
+            background: '#2B254E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 463.52,
+            height: 7.08,
+            left: 37.61,
+            top: 169.76,
+            position: 'absolute',
+            background: '#847DAA',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 49.51,
+            height: 102.97,
+            left: 254.49,
+            top: 145.95,
+            position: 'absolute',
+            background: '#60588E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 169.73,
+            height: 56.69,
+            left: 187.31,
+            top: 76.71,
+            position: 'absolute',
+            background: '#2B254E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 41.76,
+            height: 19.33,
+            left: 231.27,
+            top: 142.65,
+            position: 'absolute',
+            background: '#847DAA',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 107.95,
+            height: 84.94,
+            left: 215.1,
+            top: 66.12,
+            position: 'absolute',
+          }}
+        >
+          <div
+            style={{
+              width: 107.95,
+              height: 84.94,
+              left: 0,
+              top: 0,
+              position: 'absolute',
+              background: 'white',
+            }}
+          ></div>
+          <div
+            style={{
+              width: 107.95,
+              height: 84.94,
+              left: 4.73,
+              top: -5.41,
+              position: 'absolute',
+              transform: 'rotate(6deg)',
+              transformOrigin: '0 0',
+              background: '#FF6D5B',
+            }}
+          ></div>
+        </div>
+        <div
+          style={{
+            width: 74.36,
+            height: 27.53,
+            left: 236.53,
+            top: 89.8,
+            position: 'absolute',
+            background: 'white',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 76.04,
+            height: 38.74,
+            left: 233.83,
+            top: 97.02,
+            position: 'absolute',
+            background: '#2B254E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 38.59,
+            height: 26.98,
+            left: 256.67,
+            top: 33.13,
+            position: 'absolute',
+            background: '#60588E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 169.73,
+            height: 71.76,
+            left: 187.31,
+            top: 37.18,
+            position: 'absolute',
+          }}
+        >
+          <div
+            style={{
+              width: 169.73,
+              height: 71.76,
+              left: 0,
+              top: 0,
+              position: 'absolute',
+              background: 'white',
+            }}
+          ></div>
+          <div
+            style={{
+              width: 169.73,
+              height: 71.76,
+              left: 0,
+              top: 0,
+              position: 'absolute',
+              background: '#60588E',
+            }}
+          ></div>
+          <div
+            style={{
+              width: 44.69,
+              height: 34.19,
+              left: 49.33,
+              top: 1.71,
+              position: 'absolute',
+            }}
+          >
+            <div
+              style={{
+                width: 44.69,
+                height: 34.19,
+                left: 0,
+                top: 0,
+                position: 'absolute',
+                background: 'white',
+              }}
+            ></div>
+            <div
+              style={{
+                width: 44.69,
+                height: 34.19,
+                left: 0,
+                top: 0,
+                position: 'absolute',
+                background: '#2B254E',
+              }}
+            ></div>
+            <div
+              style={{
+                width: 36.14,
+                height: 31.26,
+                left: 15.63,
+                top: 6.59,
+                position: 'absolute',
+                background: '#FFD25B',
+              }}
+            ></div>
+          </div>
+        </div>
+        <div
+          style={{
+            width: 40.05,
+            height: 40.05,
+            left: 65.45,
+            top: 17.4,
+            position: 'absolute',
+            background: '#EBEEF6',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 27.11,
+            height: 26.62,
+            left: 113.8,
+            top: 90.67,
+            position: 'absolute',
+            background: '#FFD25B',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 27.11,
+            height: 26.62,
+            left: 364.37,
+            top: 37.43,
+            position: 'absolute',
+            background: '#FFD25B',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 20.51,
+            height: 20.03,
+            left: 391.23,
+            top: 132.18,
+            position: 'absolute',
+            background: '#FFD25B',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 18.07,
+            height: 17.58,
+            left: 169.48,
+            top: 36.21,
+            position: 'absolute',
+            background: '#FFD25B',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 335.75,
+            height: 55.81,
+            left: 104.52,
+            top: 219.43,
+            position: 'absolute',
+            background: '#847DAA',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 327.06,
+            height: 26.89,
+            left: 107.68,
+            top: 218.33,
+            position: 'absolute',
+            background: '#60588E',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 324.1,
+            height: 34.63,
+            left: 109.53,
+            top: 231.1,
+            position: 'absolute',
+            background: 'white',
+          }}
+        ></div>
+        <div
+          style={{
+            width: 303.4,
+            height: 55.49,
+            left: 119.88,
+            top: 186.94,
+            position: 'absolute',
+            background: '#2B254E',
+          }}
+        ></div>
+      </div>
+      <div
+        style={{
+          left: 577,
+          top: 1039,
+          position: 'absolute',
+          textAlign: 'center',
+        }}
+      >
+        <span
+          style={{
+            color: '#BDBDBD',
+            fontSize: 14,
+            fontFamily: 'Montserrat',
+            fontWeight: '500',
+            wordWrap: 'break-word',
+          }}
+        >
+          created by{' '}
+        </span>
+        <span
+          style={{
+            color: '#BDBDBD',
+            fontSize: 14,
+            fontFamily: 'Montserrat',
+            fontWeight: '700',
+            textDecoration: 'underline',
+            wordWrap: 'break-word',
+          }}
+        >
+          username
+        </span>
+        <span
+          style={{
+            color: '#BDBDBD',
+            fontSize: 14,
+            fontFamily: 'Montserrat',
+            fontWeight: '700',
+            wordWrap: 'break-word',
+          }}
+        >
+          {' '}
+        </span>
+        <span
+          style={{
+            color: '#BDBDBD',
+            fontSize: 14,
+            fontFamily: 'Montserrat',
+            fontWeight: '500',
+            wordWrap: 'break-word',
+          }}
+        >
+          - devChallenges.io
+        </span>
       </div>
     </div>
-    <div style={{width: 40.05, height: 40.05, left: 65.45, top: 17.40, position: 'absolute', background: '#EBEEF6'}}></div>
-    <div style={{width: 27.11, height: 26.62, left: 113.80, top: 90.67, position: 'absolute', background: '#FFD25B'}}></div>
-    <div style={{width: 27.11, height: 26.62, left: 364.37, top: 37.43, position: 'absolute', background: '#FFD25B'}}></div>
-    <div style={{width: 20.51, height: 20.03, left: 391.23, top: 132.18, position: 'absolute', background: '#FFD25B'}}></div>
-    <div style={{width: 18.07, height: 17.58, left: 169.48, top: 36.21, position: 'absolute', background: '#FFD25B'}}></div>
-    <div style={{width: 335.75, height: 55.81, left: 104.52, top: 219.43, position: 'absolute', background: '#847DAA'}}></div>
-    <div style={{width: 327.06, height: 26.89, left: 107.68, top: 218.33, position: 'absolute', background: '#60588E'}}></div>
-    <div style={{width: 324.10, height: 34.63, left: 109.53, top: 231.10, position: 'absolute', background: 'white'}}></div>
-    <div style={{width: 303.40, height: 55.49, left: 119.88, top: 186.94, position: 'absolute', background: '#2B254E'}}></div>
-  </div>
-  <div style={{left: 577, top: 1039, position: 'absolute', textAlign: 'center'}}><span style="color: '#BDBDBD', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '500', wordWrap: 'break-word'">created by </span><span style="color: '#BDBDBD', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '700', textDecoration: 'underline', wordWrap: 'break-word'">username</span><span style="color: '#BDBDBD', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '700', wordWrap: 'break-word'"> </span><span style="color: '#BDBDBD', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '500', wordWrap: 'break-word'">- devChallenges.io</span></div>
-</div>
   );
 }
 
